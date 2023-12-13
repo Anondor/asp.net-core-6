883edc2e559de57f8cd483e700d71f9235e97922
Anondor <islamazizul0000@gmail.com>
2023-12-13 13:57:59 +0600

    chapter 2 & 3

---
+++ FirstProject/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using FirstProject.Models;
+
+namespace FirstProject.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
